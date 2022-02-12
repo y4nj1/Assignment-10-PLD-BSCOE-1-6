428131a32e7df861687311183f0e4fe1496508e2
Gian Carlo Estrella <92629492+y4nj1@users.noreply.github.com>
2022-02-12 14:41:30 +0800

    Scans the created QR Code using webcam.

--- qrmakerxscanner.py
+++ qrmakerxscanner.py
@@ -1,3 +1,4 @@
+# Create QR Code
 import qrcode
 qr = qrcode.QRCode(
     version=1,
@@ -9,3 +10,22 @@ qr.add_data("https://github.com/y4nj1")
 qr.make(fit=True)
 img = qr.make_image(fill_color="black", back_color="white")
 img.save("sampleqr.png")
+
+# QR Code Scanner
+import cv2
+import webbrowser
+
+vidcap = cv2.VideoCapture(0)
+detector = cv2.QRCodeDetector()
+while True:
+    _, img = vidcap.read()
+    data, one, _ = detector.detectAndDecode(img)
+    if data:
+        read = data
+        break
+    cv2.imshow('QRCode Scanner', img)
+    if cv2.waitKey(1)==ord('a'):
+        break
+redirect = webbrowser.open((str(read)))
+vidcap.release(read)
+cv2.destroyAllWindows
