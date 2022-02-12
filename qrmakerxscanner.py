81f15e6ab35eb4465c4cabd66014a32dd4c73537
Gian Carlo Estrella <92629492+y4nj1@users.noreply.github.com>
2022-02-12 23:26:38 +0800

    Scans the QR Code with webcam; records the date and time of capture.

--- qrmakerxscanner.py
+++ qrmakerxscanner.py
@@ -1,3 +1,12 @@
+# Assignment 10
+
+# Contact Tracing App
+#	- Create a python program that will read QRCode using your webcam
+#	- You may use any online QRCode generator to create QRCode
+#	- All personal data are in QRCode 
+#	- You may decide which personal data to include
+#	- All data read from QRCode should be stored in a text file including the date and time it was read
+
 # Create QR Code
 import qrcode
 qr = qrcode.QRCode(
