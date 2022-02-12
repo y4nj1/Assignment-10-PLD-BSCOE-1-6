2d0258c78513dacec3a81749a7dca5d034077c74
Gian Carlo Estrella <92629492+y4nj1@users.noreply.github.com>
2022-02-12 14:30:02 +0800

    Creates the desired QR Code with the given link.

---
+++ qrmakerxscanner.py
@@ -0,0 +1,11 @@
+import qrcode
+qr = qrcode.QRCode(
+    version=1,
+    error_correction=qrcode.constants.ERROR_CORRECT_L,
+    box_size=10,
+    border=4,
+)
+qr.add_data("https://github.com/y4nj1")
+qr.make(fit=True)
+img = qr.make_image(fill_color="black", back_color="white")
+img.save("sampleqr.png")
